55841f82af65bb7007c1847ab29d1a28cd1a090d
mouhcinemahfoud <mouhcinemahfoud@gmail.com>
2025-02-06 11:23:46 +0100

    Add theme toggle with dark, light, and system modes
    Integrates a theme toggle button using the `@vueuse/core` and `@iconify/vue` dependencies. Updates styles and transitions for a seamless user experience. Includes additional package updates necessary for implementation.

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -33,7 +33,13 @@ import {
     TrendingUpDown,
     Users,
 } from 'lucide-vue-next';
+import { Icon } from '@iconify/vue';
 import Link from '@/Components/Link.vue';
+
+import { useColorMode } from '@vueuse/core';
+
+// Pass { disableTransition: false } to enable transitions
+const mode = useColorMode();
 </script>
 
 <template>
@@ -245,6 +251,32 @@ import Link from '@/Components/Link.vue';
                         </div>
                     </form>
                 </div>
+                <DropdownMenu>
+                    <DropdownMenuTrigger as-child>
+                        <Button variant="outline">
+                            <Icon
+                                icon="radix-icons:moon"
+                                class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+                            />
+                            <Icon
+                                icon="radix-icons:sun"
+                                class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+                            />
+                            <span class="sr-only">Toggle theme</span>
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuItem @click="mode = 'light'">
+                            Light
+                        </DropdownMenuItem>
+                        <DropdownMenuItem @click="mode = 'dark'">
+                            Dark
+                        </DropdownMenuItem>
+                        <DropdownMenuItem @click="mode = 'auto'">
+                            System
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
                 <DropdownMenu>
                     <DropdownMenuTrigger as-child>
                         <Button
