456ec40411fb9595762bf95a5c2f54b82a2e779b
mouhcinemahfoud <mouhcinemahfoud@gmail.com>
2025-02-11 09:41:02 +0100

    Remove "Upgrade to Pro" card from AuthenticatedLayout
    The "Upgrade to Pro" promotional card was removed to declutter the layout and improve user focus on core navigation elements. This simplifies the UI and enhances the overall user experience.

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -149,20 +149,6 @@ const mode = useColorMode();
                         </Link>
                     </nav>
                 </div>
-                <div class="mt-auto p-4">
-                    <Card>
-                        <CardHeader class="p-2 pt-0 md:p-4">
-                            <CardTitle>Upgrade to Pro</CardTitle>
-                            <CardDescription>
-                                Unlock all features and get unlimited access to
-                                our support team.
-                            </CardDescription>
-                        </CardHeader>
-                        <CardContent class="p-2 pt-0 md:p-4 md:pt-0">
-                            <Button size="sm" class="w-full"> Upgrade</Button>
-                        </CardContent>
-                    </Card>
-                </div>
             </div>
         </div>
         <div class="flex flex-col">
